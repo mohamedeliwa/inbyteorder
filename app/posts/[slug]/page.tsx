49354885f72bf8be2285f0f7a8bfb093269639f2
mohamedeliwa <mohamed.av11@gmail.com>
2025-12-09 00:18:32 +0200

    using module css to customize styling for the article pages

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import posts from "@/content/posts/index.json";
+import markdownStyles from "@/styles/markdown.module.css";
 
 export function generateStaticParams() {
   return posts.map(({ slug }) => ({
@@ -15,5 +16,9 @@ export default async function Post({
 }) {
   const { slug } = await params;
   const { default: Post } = await import(`@/content/posts/${slug}.mdx`);
-  return <Post />;
+  return (
+    <div className={`pt-25 ${markdownStyles["markdown"]}`}>
+      <Post />
+    </div>
+  );
 }
